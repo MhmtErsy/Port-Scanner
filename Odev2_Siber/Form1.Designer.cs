0378856fa4281aac68858eff3fbc39476c65377a
Mehmet <mehmet.ersoy1418@gmail.com>
2017-07-03 16:26:25 +0300

    PortScanner

---
+++ Odev2_Siber/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Odev2_Siber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnTara = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.lstPortlar = new System.Windows.Forms.ListBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.txtBitPort = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.txtBasPort = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtSonuc = new System.Windows.Forms.RichTextBox();
+            this.btnMacArp = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.txtHostname = new System.Windows.Forms.TextBox();
+            this.btnPing = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tabControl1.Location = new System.Drawing.Point(12, 163);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1104, 478);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.Tag = "";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Black;
+            this.tabPage1.Controls.Add(this.btnTara);
+            this.tabPage1.Controls.Add(this.pictureBox4);
+            this.tabPage1.Controls.Add(this.txtIP);
+            this.tabPage1.Controls.Add(this.lstPortlar);
+            this.tabPage1.Controls.Add(this.pictureBox3);
+            this.tabPage1.Controls.Add(this.txtBitPort);
+            this.tabPage1.Controls.Add(this.pictureBox2);
+            this.tabPage1.Controls.Add(this.txtBasPort);
+            this.tabPage1.Location = new System.Drawing.Point(4, 38);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1096, 436);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Port Scanner";
+            // 
+            // btnTara
+            // 
+            this.btnTara.BackColor = System.Drawing.SystemColors.InfoText;
+            this.btnTara.BackgroundImage = global::Odev2_Siber.Properties.Resources._5d47d41af38cd20cc76ecbb15763cd44;
+            this.btnTara.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnTara.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTara.Location = new System.Drawing.Point(16, 266);
+            this.btnTara.Name = "btnTara";
+            this.btnTara.Size = new System.Drawing.Size(480, 123);
+            this.btnTara.TabIndex = 7;
+            this.btnTara.UseVisualStyleBackColor = false;
+            this.btnTara.Click += new System.EventHandler(this.btnTara_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Odev2_Siber.Properties.Resources._1fb2e9b35bb25708cfa4652762155843;
+            this.pictureBox4.Location = new System.Drawing.Point(16, 33);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(183, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(205, 33);
+            this.txtIP.Multiline = true;
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(291, 50);
+            this.txtIP.TabIndex = 5;
+            // 
+            // lstPortlar
+            // 
+            this.lstPortlar.FormattingEnabled = true;
+            this.lstPortlar.ItemHeight = 29;
+            this.lstPortlar.Location = new System.Drawing.Point(521, 33);
+            this.lstPortlar.Name = "lstPortlar";
+            this.lstPortlar.Size = new System.Drawing.Size(548, 352);
+            this.lstPortlar.TabIndex = 4;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Odev2_Siber.Properties.Resources._291164463b003cffed0cad6c52802d74;
+            this.pictureBox3.Location = new System.Drawing.Point(16, 185);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(368, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // txtBitPort
+            // 
+            this.txtBitPort.Location = new System.Drawing.Point(390, 185);
+            this.txtBitPort.Multiline = true;
+            this.txtBitPort.Name = "txtBitPort";
+            this.txtBitPort.Size = new System.Drawing.Size(106, 50);
+            this.txtBitPort.TabIndex = 2;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Odev2_Siber.Properties.Resources._08d0dfefc09b52ecb9c78bd4c395a4c7;
+            this.pictureBox2.Location = new System.Drawing.Point(16, 108);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(368, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // txtBasPort
+            // 
+            this.txtBasPort.Location = new System.Drawing.Point(390, 108);
+            this.txtBasPort.Multiline = true;
+            this.txtBasPort.Name = "txtBasPort";
+            this.txtBasPort.Size = new System.Drawing.Size(106, 50);
+            this.txtBasPort.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Black;
+            this.tabPage2.Controls.Add(this.txtSonuc);
+            this.tabPage2.Controls.Add(this.btnMacArp);
+            this.tabPage2.Location = new System.Drawing.Point(4, 38);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1096, 436);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Mac / Arp Scanner";
+            // 
+            // txtSonuc
+            // 
+            this.txtSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSonuc.Location = new System.Drawing.Point(34, 131);
+            this.txtSonuc.Name = "txtSonuc";
+            this.txtSonuc.Size = new System.Drawing.Size(1028, 271);
+            this.txtSonuc.TabIndex = 10;
+            this.txtSonuc.Text = "";
+            // 
+            // btnMacArp
+            // 
+            this.btnMacArp.BackColor = System.Drawing.SystemColors.InfoText;
+            this.btnMacArp.BackgroundImage = global::Odev2_Siber.Properties.Resources._1414e24a3fa1ef4a2098314ed53ff98b;
+            this.btnMacArp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnMacArp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMacArp.Location = new System.Drawing.Point(34, 17);
+            this.btnMacArp.Name = "btnMacArp";
+            this.btnMacArp.Size = new System.Drawing.Size(1028, 108);
+            this.btnMacArp.TabIndex = 9;
+            this.btnMacArp.UseVisualStyleBackColor = false;
+            this.btnMacArp.Click += new System.EventHandler(this.btnMacArp_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.Black;
+            this.tabPage3.Controls.Add(this.lblDurum);
+            this.tabPage3.Controls.Add(this.txtHostname);
+            this.tabPage3.Controls.Add(this.btnPing);
+            this.tabPage3.Location = new System.Drawing.Point(4, 38);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(1096, 436);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Ping Tool";
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.ForeColor = System.Drawing.Color.Red;
+            this.lblDurum.Location = new System.Drawing.Point(24, 220);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(21, 29);
+            this.lblDurum.TabIndex = 9;
+            this.lblDurum.Text = "-";
+            // 
+            // txtHostname
+            // 
+            this.txtHostname.Location = new System.Drawing.Point(31, 23);
+            this.txtHostname.Name = "txtHostname";
+            this.txtHostname.Size = new System.Drawing.Size(1032, 34);
+            this.txtHostname.TabIndex = 0;
+            // 
+            // btnPing
+            // 
+            this.btnPing.BackColor = System.Drawing.SystemColors.InfoText;
+            this.btnPing.BackgroundImage = global::Odev2_Siber.Properties.Resources._254d591c3bfc7909fdd505002c8b2902;
+            this.btnPing.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnPing.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPing.Location = new System.Drawing.Point(31, 83);
+            this.btnPing.Name = "btnPing";
+            this.btnPing.Size = new System.Drawing.Size(1032, 111);
+            this.btnPing.TabIndex = 8;
+            this.btnPing.UseVisualStyleBackColor = false;
+            this.btnPing.Click += new System.EventHandler(this.btnPing_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Odev2_Siber.Properties.Resources._6ab63f7cad870243ad9d396849f33ac7;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1096, 104);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1128, 653);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Siber Güvenlik - Ödev 2";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txtBasPort;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox txtBitPort;
+        private System.Windows.Forms.Button btnTara;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.ListBox lstPortlar;
+        private System.Windows.Forms.Button btnPing;
+        private System.Windows.Forms.TextBox txtHostname;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.Button btnMacArp;
+        private System.Windows.Forms.RichTextBox txtSonuc;
+    }
+}
+
---
+++ Odev2_Siber/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+
+
+namespace Odev2_Siber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int control = 0;
+        private void btnTara_Click(object sender, EventArgs e)
+        {
+            lstPortlar.Text = "";
+            control = int.Parse(txtBasPort.Text);
+        etiket:
+            try
+            {
+                TcpClient tcp = new TcpClient();
+                control = control + 1;
+                tcp.Connect(txtIP.Text, control);
+                
+                if (tcp.Connected)
+                {
+                    if (control != int.Parse(txtBitPort.Text)+1)
+                    {
+
+                        lstPortlar.Items.Add(control.ToString()+ " PORTU AÇIK");
+
+                        goto etiket;
+
+                    }
+                }
+            }
+            catch
+            {
+                if (control != int.Parse(txtBitPort.Text)+1)
+                {
+
+                    lstPortlar.Items.Add(control.ToString() + " PORTU KAPALI");
+                    
+                    goto etiket;
+                }
+                else
+                {
+                }
+            }
+            
+            
+            
+        }
+        private void btnPing_Click(object sender, EventArgs e)
+        {
+            bool pinging = false;
+
+            Ping p = new Ping();
+            try
+            {
+                PingReply reply = p.Send(txtHostname.Text);
+                pinging = reply.Status == IPStatus.Success;
+            }
+            catch (PingException pe)
+            {
+                MessageBox.Show(pe.Message.ToString());
+            }
+            if (pinging)
+                lblDurum.Text = txtHostname.Text + " is Alive....";
+            else
+                lblDurum.Text = txtHostname.Text + " is Dead....";
+        }
+
+        private void btnMacArp_Click(object sender, EventArgs e)
+        {
+            string TEMP = "";
+            foreach (NetworkInterface NI in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                IPInterfaceProperties IPPS = NI.GetIPProperties();
+                TEMP += "CİHAZ BİLGİSİ: "+Environment.NewLine;
+                foreach (UnicastIPAddressInformation ADRS in IPPS.UnicastAddresses)
+                {
+                   TEMP += ADRS.Address.ToString() + " MAC: "+NI.GetPhysicalAddress()+Environment.NewLine;
+                   
+                }
+                TEMP += Environment.NewLine;
+            }
+            txtSonuc.Text = TEMP;
+           
+
+        }
+
+        private void btnicmbps_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
